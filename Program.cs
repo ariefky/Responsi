a6a79a01ad4df84feb31ddd907b8de3f6d477cb2
ariefky <48465565+ariefky@users.noreply.github.com>
2019-04-27 18:07:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krywn = new karyawan();
+
+            krywn.nama = "Arief";
+            krywn.nik = "1357";
+            krywn.gaji = 1000000;
+
+            krywn.nama2 = "Kiky";
+            krywn.nik2 = "2468";
+            krywn.gaji2 = 2000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
